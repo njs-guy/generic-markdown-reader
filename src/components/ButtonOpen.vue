485256f6a081175188e0ef983170ca654ff4953e
Nick Shockey <shockeynj@gmail.com>
2022-05-12 13:14:31 -0400

    ButtonOpen.vue now uses JS
    Mainly because I have no idea why line 25 gives an error
    Simply changing the file to JS fixes it
    This file doesn't even need to be TS anyway so this is easier

--- src/components/ButtonOpen.vue
+++ src/components/ButtonOpen.vue
@@ -6,7 +6,7 @@
     <!-- @btn-open-click="$emit('btn-open-click')" -->
 </template>
 
-<script lang="ts">
+<script>
 import Vue, { defineComponent } from 'vue';
 import StdButton from "./Button.vue";
 
@@ -22,14 +22,13 @@ export default defineComponent({
         // With FileReader, take the opened file and read the text as a string. 
         // After it's been loaded, emit "loadedText" along with the text string.
         openText() {
-            const file = this.$refs.btnOpen.files[0]; // Despite this being an error, this line works perfectly. (shrug)
+            const file = this.$refs.btnOpen.files[0]; // Despite this being an error in TS, this line works perfectly. (shrug)
             const reader = new FileReader();
             reader.onload = (e) => {
                 let text = String(e.target?.result);
                 this.$emit("loadedText", text);
             };
             reader.readAsText(file);
-            
         }
     },
 })
